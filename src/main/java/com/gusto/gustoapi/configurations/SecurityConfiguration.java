903c95c338fc847facba94b3ba7c76ff97c6f825
ElizabethD3 <elizabeth.diaz@jmir.org>
2024-01-09 13:06:33 -0500

    clean up

--- src/main/java/com/gusto/gustoapi/configurations/SecurityConfiguration.java
+++ src/main/java/com/gusto/gustoapi/configurations/SecurityConfiguration.java
@@ -1,8 +1,6 @@
 package com.gusto.gustoapi.configurations;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
